f636ed51db1e3992459b94b8d56820887453e6ae
fluegergehaimer <saymur4.0@gmail.com>
2024-03-28 12:35:23 +0000

    it works

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['52.250.100.202', '127.0.0.1', 'localhost', 'trykittygramforyap.hopto.org']
 
@@ -94,10 +94,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static_backend/'
-STATIC_ROOT = BASE_DIR/ 'static_backend'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'var/www/kittygram/media'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
